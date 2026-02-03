3443618ea9a049786e11a53bbd3c6982514e50c2
768650-cell <768650@student.dusd.net>
2026-02-03 15:28:04 -0800

    Player Control project
    I start my second script.

---
+++ P7PlayerControlSingh/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // We'11 move the vehical forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
