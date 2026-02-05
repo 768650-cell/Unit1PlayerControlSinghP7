61c9af8412f818e5e1514276c03d12ad7a09f004
768650-cell <768650@student.dusd.net>
2026-02-05 15:27:33 -0800

    Player Control
    I am doing my script of pLAYERcONTROL.

--- P7PlayerControlSingh/Assets/Scripts/PlayerController.cs
+++ P7PlayerControlSingh/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +15,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Verttical");
         // We'11 move the vehical forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+            
+
+            
     }
 }
