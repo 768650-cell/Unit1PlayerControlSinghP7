fa0ce1d2665bb4df3ac40c5fab93962b69fc5e17
768650-cell <768650@student.dusd.net>
2026-02-25 23:05:14 -0800

    Player Control Project.
    I done the project.

--- P7PlayerControlSingh/Assets/Scripts/PlayerControl.cs
+++ P7PlayerControlSingh/Assets/Scripts/PlayerControl.cs
@@ -2,22 +2,31 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
+
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+    private float speed = 8.0f;
+    private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
-
-   
 
     // Update is called once per frame
     void Update()
     {
+        // This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        // Moves the car forward based on vertical input
+
+        // We move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        // Rotate the car based on horizontal input
-        transform.Translate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-       
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
