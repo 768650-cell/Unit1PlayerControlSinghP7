fceaa2a6be3977ad513f83ff5508f3485c963240
768650-cell <768650@student.dusd.net>
2026-02-26 22:38:25 -0800

    Player Control Project
    I done by the challenge.

--- P7PlayerControlSingh/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ P7PlayerControlSingh/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(0, 0, 0);
+    private Vector3 offset = new Vector3(9, 1, -3);
 
     // Start is called before the first frame update
     void Start()
--- P7PlayerControlSingh/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ P7PlayerControlSingh/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -26,6 +26,6 @@ public class PlayerControllerX : MonoBehaviour
      
         transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.up * rotationSpeed* verticalInput * Time.deltaTime);
+        transform.Rotate(Vector3.left * rotationSpeed* verticalInput * Time.deltaTime);
     }
 }
